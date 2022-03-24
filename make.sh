a0fa15f82a7134355d2d9c31d4a65be1390b5873
0xol <86998622+0xol@users.noreply.github.com>
2022-03-25 01:15:50 +1000

    make.sh

--- make.sh
+++ make.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-dir
+
 IMGSIZE="10" 
 
 export LFS=/mnt/lfs
 
+echo $LFS
 python getSRC.py
+echo script isnt stuck, its just making the disk image
 touch bin/linuxBootable.img
 sudo dd if=/dev/zero of=bin/linuxBootable.img bs=1G count=$IMGSIZE
-
+mkfs -v -t ext4 bin/linuxBootable.img
+sudo mkdir -pv $LFS
+sudo mount -v -t ext4 bin/linuxBootable.img $LFS
+sudo mkdir -v $LFS/sources
+sudo chmod -v a+wt $LFS/source
+wget -O sources/packages.tar http://ftp.lfs-matrix.net/pub/lfs/lfs-packages/lfs-packages-11.1.tar
